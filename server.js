60b3c3fdc04c0bb67b4b8e22140571a92bcb18cc
Vanshita1011 <vanshitapatel400@gmail.com>
2025-04-21 13:09:46 +0530

    availibility

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/api/admin", admin); // Import adminRoutes);
 app.use("/api/doctors", doctorRoutes); //  doctor routes
 app.use("/api/slots", slotRoutes);
 app.use("/api/queries", queryRoutes);
-app.use("/api/availibility", availabilityRoutes);
+app.use("/api/availability", availabilityRoutes);
 
 const Image = mongoose.model("Image", { Img_Url: String });
 app.post("/", upload.single("img"), async (req, res) => {
